d67968b7ab93cc90069269d580223651dd6306f7
Gus <garciadelacruzgustavo@gmail.com>
2024-01-08 15:24:29 -0600

    feat(api): add basic configuration

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,17 @@
+import cors from 'cors';
 import express from 'express';
+import helmet from 'helmet';
+
+import { corsOptions } from '../config/corsConfig';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello MFEE!' });
