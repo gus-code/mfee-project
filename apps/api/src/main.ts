44fbb8800cd880caac482269114d7cc745f8bc41
Gus <garciadelacruzgustavo@gmail.com>
2024-01-07 22:48:42 -0600

    chore(api): add initial structure

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,7 +6,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+  res.send({ message: 'Hello MFEE!' });
 });
 
 app.listen(port, host, () => {
