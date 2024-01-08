505f33416da05e67fc551bd6e4ffd1c74e1bf75e
Gus <garciadelacruzgustavo@gmail.com>
2024-01-08 11:55:57 -0600

    feat(api): add route to main file

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 
+import auth from './routes/auth';
 import categories from './routes/categories';
 
 const host = process.env.HOST ?? 'localhost';
@@ -8,6 +9,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = express();
 
 app.use(express.json());
+app.use('/api/auth', auth);
 app.use('/api/categories', categories);
 
 app.listen(port, host, () => {
