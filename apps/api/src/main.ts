e7c7b0ce9520aeaa81e0d4100487232b65add0d0
Fany <35381008+esthefanyMR@users.noreply.github.com>
2024-01-24 22:43:33 -0600

    adding route object for posts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,7 @@ import helmet from 'helmet';
 import { corsOptions } from './config/corsConfig';
 
 import categories from './routes/categories';
-
+import posts from './routes/posts';
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
@@ -16,6 +16,7 @@ app.use(helmet());
 app.use(cors(corsOptions));
 
 app.use('/api/categories', categories);
+app.use('/api/posts', posts);
 
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
