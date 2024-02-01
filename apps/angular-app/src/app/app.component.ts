5d74c11c611d5ed98879c651bebb970fcecc0d29
Mauricio Delgado <mauricio_delgado10@hotmail.com>
2024-02-01 15:19:15 -0600

    Session 01

--- apps/angular-app/src/app/app.component.ts
+++ apps/angular-app/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  standalone: true,
-  imports: [RouterModule],
+  // standalone: true,
+  //imports: [RouterModule ],
   selector: 'mfee-project-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
