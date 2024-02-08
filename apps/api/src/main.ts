5fbccb37118e7d7bd6df660a798a81c6a84ad1eb
Araujo <fernando.araujo@accenture.com>
2024-02-08 15:38:02 -0600

    Moongose conenction and changing post schema

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,6 +8,7 @@ import { verifyToken } from './middleware/auth';
 import { errorHandler } from './middleware/errorHandler';
 import auth from './routes/auth';
 import categories from './routes/categories';
+import posts from './routes/posts';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
@@ -22,7 +23,7 @@ app.use('/api/auth', auth);
 
 app.use(verifyToken);
 app.use('/api/categories', categories);
-
+app.use('/api/posts', posts);
 app.use(errorHandler);
 
 mongoose
@@ -31,9 +32,14 @@ mongoose
     console.log('Connected to MongoDB');
 
     app.listen(port, host, () => {
-      console.log(`[ ready ] http://${host}:${port}`);
+      console.log(`Express Server started...  [ ready ] http://${host}:${port}`);
     });
   })
   .catch((e) => {
     console.error(e);
+  });
+
+  mongoose.connection.on('error', (err) => {
+    console.log(`DB connection runtime error`);
+    console.log(err);
   });
--- apps/api/src/models/category.ts
+++ apps/api/src/models/category.ts
@@ -1,6 +1,6 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
-interface ICategory extends Document {
+export interface ICategory extends Document {
   name: string;
 }
 
