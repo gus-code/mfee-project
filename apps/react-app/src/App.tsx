67faa00728579c10469dc5b9811895266cd19b76
Alan Rincon <alan.rnca@gmail.com>
2024-03-07 12:12:58 -0600

    Commit 2

--- apps/react-app/src/App.tsx
+++ apps/react-app/src/App.tsx
@@ -8,24 +8,11 @@ import Header from './components/Header';
 function App() {
   return (
     <Grid container id="app" direction="column" height="100vh" flexWrap="nowrap">
-      {/* Activity 2 - Move all Container content to Header component */}
       <Container container>
-        <Grid item flexGrow={1}>
-          <Typography variant="caption" color="primary" alignItems="center">
-            <span style={{ fontSize: '1.5rem' }}>[ </span>
-            Making your Life Easier
-            <span style={{ fontSize: '1.5rem' }}> ]</span>
-          </Typography>
-        </Grid>
-        <Grid item flexGrow={1}>
-          <Typography variant="h4" fontWeight="bold">
-            Discovering the World
-          </Typography>
-        </Grid>
+        <Header></Header>
       </Container>
 
       <Grid item flexGrow={1}>
-        {/* Activity 1 - Render HomePage and PostPage */}
         <HomePage></HomePage>
         <PostPage></PostPage>
       </Grid>
--- apps/react-app/src/components/Header/Header.tsx
+++ apps/react-app/src/components/Header/Header.tsx
@@ -1,1 +1,22 @@
+import { Grid, Typography } from '@mui/material';
 
+function Header() {
+  return (
+    <>
+      <Grid item flexGrow={1}>
+        <Typography variant="caption" color="primary" alignItems="center">
+          <span style={{ fontSize: '1.5rem' }}>[ </span>
+          Making your Life Easier
+          <span style={{ fontSize: '1.5rem' }}> ]</span>
+        </Typography>
+      </Grid>
+      <Grid item flexGrow={1}>
+        <Typography variant="h4" fontWeight="bold">
+          Discovering the World
+        </Typography>
+      </Grid>
+    </>
+  );
+}
+
+export default Header;
