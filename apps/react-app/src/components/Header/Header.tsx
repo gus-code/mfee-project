1c21faa1158f08053e8cf7631cd845b2e55c867c
Brenda Molina <brenda.molina96@hotmail.com>
2024-03-08 01:21:12 -0600

    Activity 2

--- apps/react-app/src/components/Header/Header.tsx
+++ apps/react-app/src/components/Header/Header.tsx
@@ -1,1 +1,23 @@
 
+import { Grid, Typography } from '@mui/material';
+
+import { Container } from './Header.styles';
+
+function Header() {
+    
+<Container container>
+        <Grid item flexGrow={1}>
+          <Typography variant="caption" color="primary" alignItems="center">
+            <span style={{ fontSize: '1.5rem' }}>[ </span>
+            Making your Life Easier
+            <span style={{ fontSize: '1.5rem' }}> ]</span>
+          </Typography> 
+        </Grid>
+        <Grid item flexGrow={1}>
+          <Typography variant="h4" fontWeight="bold">
+            Discovering the World
+          </Typography>
+        </Grid>
+      </Container>
+}
+export default Header
