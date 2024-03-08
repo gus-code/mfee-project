dc864b3953b0998265c04574a43c04a2b087a520
Brenda Molina <brenda.molina96@hotmail.com>
2024-03-08 01:40:54 -0600

    Activity 2 -fix

--- apps/react-app/src/App.tsx
+++ apps/react-app/src/App.tsx
@@ -2,15 +2,16 @@ import { Grid } from '@mui/material';
 
 import HomePage from './components/HomePage';
 import PostPage from './components/PostPage';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <Grid container id="app" direction="column" height="100vh" flexWrap="nowrap">
-      {/* Activity 2 - Move all Container content to Header component */}
+      <Header/>
       <Grid item flexGrow={1}>
         {/* Activity 1 - Render HomePage and PostPage */}
-       <HomePage></HomePage>
-       <PostPage></PostPage>
+       <HomePage/>
+       <PostPage/>
       </Grid>
     </Grid>
   );
