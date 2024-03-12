050051ab8057b2211a8d5f66151ba6347a83bded
ABluGreen <a_blugreen@hotmail.com>
2024-03-12 18:53:33 +0000

    Actividad 2

--- apps/react-app/src/App.tsx
+++ apps/react-app/src/App.tsx
@@ -1,28 +1,13 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid } from '@mui/material';
 
-import { Container } from './components/Header/Header.styles';
+import Header from './components/Header/Header';
 import HomePage from './components/HomePage';
 import PostPage from './components/PostPage';
 
 function App() {
   return (
     <Grid container id="app" direction="column" height="100vh" flexWrap="nowrap">
-      {/* Activity 2 - Move all Container content to Header component */}
-      <Container container>
-        <Grid item flexGrow={1}>
-          <Typography variant="caption" color="primary" alignItems="center">
-            <span style={{ fontSize: '1.5rem' }}>[ </span>
-            Making your Life Easier
-            <span style={{ fontSize: '1.5rem' }}> ]</span>
-          </Typography>
-        </Grid>
-        <Grid item flexGrow={1}>
-          <Typography variant="h4" fontWeight="bold">
-            Discovering the World
-          </Typography>
-        </Grid>
-      </Container>
-
+      <Header/>
       <Grid item flexGrow={1}>
         <HomePage/>
         <PostPage />
