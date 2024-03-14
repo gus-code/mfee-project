e0ebc5f96fd812560b092d6e2cdfd13dcf7b185f
Eri Mariño <erika.marino@accenture.com>
2024-03-13 22:43:57 -0600

    Actividad 5

--- apps/react-app/src/components/CategoryButtonGroup/CategoryButtonGroup.tsx
+++ apps/react-app/src/components/CategoryButtonGroup/CategoryButtonGroup.tsx
@@ -32,18 +32,15 @@ function CategoryButtonGroup({ categorySelected, handleSelectCategory }: Categor
   return (
     <Container item>
       <ButtonGroup aria-label="category button group" color="inherit">
-        {/* Activity 5 - Iterate categoryOptions */}
         {categoryOptions.map((option) => (
           <StyledButton
             key={option.key}
             type="button"
-            // Activity 5 - Replaces the hardcoded value "categoryOptions[0].name" with the variable obtained from the iteration and uses the variable "categorySelected" obtained from the props
             selected={option.name === categorySelected}
             onClick={() => {
-              //  Activity 5 - After you have iterated the "categoryOptions" array, send the "name" property as a parameter to the "handleSelectCategory" function
+              handleSelectCategory(option.name);
             }}
           >
-            {/* Activity 5 - Render category name */}
             {option.name}
           </StyledButton>
         ))}
--- apps/react-app/src/components/Comments/Comments.tsx
+++ apps/react-app/src/components/Comments/Comments.tsx
@@ -7,15 +7,12 @@ interface CommentsProps {
 }
 
 function Comments({ postComments }: CommentsProps) {
-  const tempComment: Comment = postComments[0];
-
   return (
     <Container container>
       <Title item sm={8}>
         <h4>Comments</h4>
       </Title>
-      {/* Activity 5 - Iterate comments */}
-      {postComments.length > 0 && postComments.map((comment) => <CommentCard comment={tempComment} />)}
+      {postComments.length > 0 && postComments.map((comment) => <CommentCard key={comment.id} comment={comment} />)}
     </Container>
   );
 }
