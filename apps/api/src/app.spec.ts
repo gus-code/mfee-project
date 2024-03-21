b3efe73d5dd786ef8569fd39744ea539871b86d9
Gus <garciadelacruzgustavo@gmail.com>
2024-03-21 12:15:40 -0600

    test: add get all categories

--- apps/api/src/app.spec.ts
+++ apps/api/src/app.spec.ts
@@ -2,6 +2,12 @@ import request from 'supertest';
 
 import app from './app';
 
+jest.mock('jsonwebtoken', () => ({
+  verify: jest.fn((token, secretOrPublicKey, callback) => {
+    return callback(null, { sub: 'user_id' });
+  })
+}));
+
 describe('/api/auth', () => {
   test('it should register a user', (done) => {
     request(app)
@@ -11,7 +17,27 @@ describe('/api/auth', () => {
         password: 'Aa$123'
       })
       .then((response) => {
+        const jsonResponse = JSON.parse(response.text);
+
         expect(response.statusCode).toBe(201);
+        expect(jsonResponse.message).toEqual('User registered successfully');
+
+        done();
+      });
+  });
+});
+
+describe('/api/categories', () => {
+  test('it should get all the categories', (done) => {
+    request(app)
+      .get('/api/categories')
+      .set('authorization', 'Bearer 12345')
+      .then((response) => {
+        const jsonResponse = JSON.parse(response.text);
+
+        expect(response.statusCode).toBe(200);
+        expect(jsonResponse).toEqual([]);
+
         done();
       });
   });
