7dd5f4b67bfce33171b412c8b85047a094da8719
Alan Rincon <alan.rnca@gmail.com>
2024-03-25 17:15:51 -0600

    Fix activity 7

--- apps/react-app/src/context/SnackbarProvider.tsx
+++ apps/react-app/src/context/SnackbarProvider.tsx
@@ -1,11 +1,11 @@
-import { Alert, Snackbar } from '@mui/material';
+import { Alert, AlertColor, Snackbar } from '@mui/material';
 import { createContext, useState } from 'react';
 
 interface SnackbarContextProps {
-  createAlert: (severity: 'success' | 'error', message: string) => void;
+  createAlert: (severity: AlertColor, message: string) => void;
 }
 
-interface PostProviderProps {
+interface SnackbarProviderProps {
   children: React.JSX.Element;
 }
 
@@ -13,21 +13,32 @@ export const SnackbarContext = createContext<SnackbarContextProps>({
   createAlert: () => {}
 });
 
-export function SnackbarProvider({ children }: PostProviderProps): React.JSX.Element {
-  const [open, setOpen] = useState(true);
+export function SnackbarProvider({ children }: SnackbarProviderProps): React.JSX.Element {
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState('');
+  const [severity, setSeverity] = useState<AlertColor>('success');
 
-  function createAlert (severity: 'success' | 'error', message: string) {
+  const handleClose = (_event: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+  const createAlert = (severity: AlertColor, message: string) => {
+    setMessage(message);
+    setSeverity(severity);
     setOpen(true);
-    console.log(message);
+  };
 
-    return (
-      <Snackbar open={open} autoHideDuration={5000}>
-        <Alert severity={severity} variant="filled" sx={{ width: '100%' }}>
+  return (
+    <>
+      <Snackbar open={open} onClose={handleClose} autoHideDuration={5000} anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}>
+        <Alert onClose={handleClose} severity={severity} variant="filled" sx={{ width: '100%' }}>
           {message}
         </Alert>
       </Snackbar>
-    );
-  };
-
-  return <SnackbarContext.Provider value={{ createAlert }}>{children}</SnackbarContext.Provider>;
+      <SnackbarContext.Provider value={{ createAlert }}>{children}</SnackbarContext.Provider>
+    </>
+  );
 }
