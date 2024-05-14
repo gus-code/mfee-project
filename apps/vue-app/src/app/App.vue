66ce385b3422812523ec4e5947d1b52dc0223380
Alejandro Lagarde <c-alejandro.lagarde@charter.com>
2024-05-14 13:42:59 -0600

    Actividad 1

--- apps/vue-app/src/app/App.vue
+++ apps/vue-app/src/app/App.vue
@@ -1,4 +1,14 @@
 <template>
   <!-- Activity 1: Render PostLayout.vue  -->
-  <h1>TODO: Eliminar este h1 cuando se haga la actividad</h1>
+  <PostLayout/>
 </template>
+
+<script>
+import PostLayout from './modules/posts/layouts/PostLayout.vue'
+
+export default {
+  components: {
+    PostLayout
+  }
+}
+</script>
--- apps/vue-app/src/app/modules/posts/layouts/PostLayout.vue
+++ apps/vue-app/src/app/modules/posts/layouts/PostLayout.vue
@@ -2,3 +2,9 @@
 <template>
   <h1>TODO: Eliminar este h1 cuando se haga la actividad</h1>
 </template>
+
+<script>
+export default {
+  name: 'PostLayout'
+}
+</script>
