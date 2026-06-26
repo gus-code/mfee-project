d6334ed79efd55ddd50c0a2c039c98a1094e03ca
Yael <cruz.y.perez@accenture.com>
2026-06-26 13:27:59 -0600

    Missing componets added

---
+++ apps/react-app/src/components/Page/LoginPage/LoginPage.tsx
@@ -0,0 +1,13 @@
+import { Grid } from "@mui/material"
+import { PageContainer } from "./LoginPage.styles"
+
+export const LoginPage = () => {
+  return (
+    <PageContainer container>
+      Login Page
+      <Grid item md={4} xs={4} lg={4}>
+        Form
+      </Grid>
+    </PageContainer>
+  )
+}
